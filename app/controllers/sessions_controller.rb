9f8cac4d5339b65ed45a88f68a478757f7294ec9
amuliawijaya23 <amuliawijaya@gmail.com>
2022-05-11 17:01:14 -0700

    refactor user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:email].strip.downcase)
+    if user.authenticate_with_credentials(params[:email].strip.downcase, params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 8 }
+
+  def authenticate_with_credentials(email, password)
+    if self.authenticate(password)
+      return self
+    else
+      return nil
+    end
+  end
 
 end
